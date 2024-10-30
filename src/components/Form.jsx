f15b60722fde97c1170df9e05fb473abd8a7ad35
Dilshod Rahmatov <dilshod.rahmatov@gmail.com>
2024-10-30 11:47:28 +0100

    frontend deploy 1

---
+++ src/components/Form.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import axios from "axios";
+
+const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const userData = { name, email, password };
+
+  const [details, setDetails] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/submit",
+        userData,
+      );
+
+      setDetails(response.data.details);
+
+      setName("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(details);
+  return (
+    <div>
+      <form>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <br />
+        <input type="submit" value="Submit" onClick={handleSubmit} />
+      </form>
+
+      {details.map((user) => {
+        return (
+          <div key={user._id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Form;
